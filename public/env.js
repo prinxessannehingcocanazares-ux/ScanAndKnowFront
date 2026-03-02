cdc10352a614e3b0be6ec9ea79f0354862083143
princess <prinxessannehingcocanazares@gmail.com>
2026-03-03 01:08:33 +0800

    updated istest

--- public/env.js
+++ public/env.js
@@ -2,8 +2,8 @@ window.__ENV__ = {
   VITE_IS_TEST: "false",
 
   // Base URLS for API
-  VITE_API_URL_TEST: "https://localhost:44315",
-  VITE_API_URL_PROD: "https://yourproductionapi.com",
+  VITE_API_URL_TEST: "https://localhost:7236",
+  VITE_API_URL_PROD: "https://scanandknowback.onrender.com/",
 
   // Endpoint URLS
   VITE_SIGNUP_ENDPOINT: "/api/User/SignUp",
